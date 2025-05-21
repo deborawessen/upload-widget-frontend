e3a1e193ebc27dca3ac45b5462c500386ff4dadc
Debora Wessen <deborawessen@gmail.com>
2025-05-21 17:12:27 -0400

    feat: add collapsible upload widget and styled button with Tailwind Variants

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,19 +1,28 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button"
+import { useState } from "react";
 
 export function UploadWidget(){
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
     return (
-        <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
-            <UploadWidgetHeader/>
+        <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+            <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape">
+                {!isWidgetOpen && <UploadWidgetMinimizedButton/>}
+                <Collapsible.Content>
+                    <UploadWidgetHeader/>
 
-            <div className="flex flex-col gap-4 py-3">
-                <UploadWidgetDropzone/>
+                    <div className="flex flex-col gap-4 py-3">
+                        <UploadWidgetDropzone/>
 
-                <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
+                        <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
 
-                <UploadWidgetUploadList/>
+                        <UploadWidgetUploadList/>
+                    </div>
+                </Collapsible.Content>
             </div>
-        </div>
+        </Collapsible.Root>
     )
 }
