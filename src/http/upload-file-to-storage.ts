a016e6db202bce11332d2f0ee2fe3b1b9bc43ef3
Debora Wessen <deborawessen@gmail.com>
2025-06-04 19:55:38 -0400

    feat: add cancel and retry functionality for file uploads with status handling

--- src/http/upload-file-to-storage.ts
+++ src/http/upload-file-to-storage.ts
@@ -5,14 +5,23 @@ interface UploadFileToStorageParams {
 
 }
 
-export async function uploadFileToStorage( {file}:UploadFileToStorageParams ) {
+interface UploadFileToStorageOpts {
+    signal?: AbortSignal
+}
+
+export async function uploadFileToStorage( 
+    {file}:UploadFileToStorageParams,
+     opts?: UploadFileToStorageOpts 
+) {
     const data = new FormData()
 
     data.append('file', file)
+
     const response = await axios.post<{ url:string}>('http://localhost:3333/uploads', data, {
         headers: {
             'Content-Type': 'multipart/form-data',
-        }
+        },
+        signal: opts?.signal,
     })
 
     return {url: response.data.url}
